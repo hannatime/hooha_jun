eb2f439841a13129a69947894ab4e1864c6fca1e
Ben Hanna <ben.hanna@me.com>
2014-07-05 18:26:36 +1000

    remove seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,32 +24,9 @@ u2.skip_confirmation!
 u2.save!
 u2.add_role :gold
 
-# for crm_account in 1..100 do
-#  b = Account.new(
-#     account_name: "Account + #{crm_account}",
-#     account_phone: "12345678",
-#     user_id: "1",
-#     account_id: "2",
-# )
-#  b.save!
-# end
 
-a = Account.new(
-    account_name: "API Pharma",
-    account_phone: "04447444255",
-    user_id: "1",
-)
 
-a.save!
 
-b = Opportunity.new(
-    opportunity_name: "CRM Sale",
-    opportunity_account_name: "API Pharma",
-    opportunity_owner: "Ben Hanna",
-    user_id: "1",
-    account_id: "1",
-)
-b.save!
 
 
   
