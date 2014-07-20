bb732f174d8eec8651330a4800a85c91c400b184
Ben Hanna <ben.hanna@me.com>
2014-07-20 20:46:44 +1000

    associations

--- db/migrate/20140720084949_create_customers_opportunities_join.rb
+++
@@ -1,8 +0,0 @@
-class CreateCustomersOpportunitiesJoin < ActiveRecord::Migration
-  def change
-    create_table :customers_opportunities, :id => false do |t|
-    t.column :customer_id, :integer
-    t.column :opportunity_id, :integer
-    end
-  end
-end
