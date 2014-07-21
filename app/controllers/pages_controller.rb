08a070f5c2a6ba39770b04cb7ed9aa26b1d2cd78
Ben Hanna <ben.hanna@me.com>
2014-07-21 20:03:38 +1000

    useability

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,18 +12,18 @@ class PagesController < ApplicationController
 
   def dashboard
 
-      @q = Task.accessible_by(current_ability).search(params[:q])
-      @tasks1 = @q.result(distinct: true).page params[:page]
-
-      @q = Opportunity.accessible_by(current_ability).search(params[:q])
-      @opportunities1 = @q.result(distinct: true).page params[:page]
-    # @tasks1 = Task.accessible_by(current_ability).order(:task_due_date).page params[:page]
+    @q = Task.accessible_by(current_ability).search(params[:q])
+    @tasks1 = @q.result(distinct: true).page params[:page]
+  
+    @q = Opportunity.accessible_by(current_ability).search(params[:q])
+    @opportunities1 = @q.result(distinct: true).page params[:page]
+  
     @tasks = Task.accessible_by(current_ability)
-    #@opportunities1 = Opportunity.accessible_by(current_ability).order(:opportunity_closing_date).page params[:page]
     @opportunities = Opportunity.accessible_by(current_ability)
     @customers = Customer.accessible_by(current_ability)
     @accounts = Account.accessible_by(current_ability)
     @users = User.accessible_by(current_ability)
+    
   end 
 
   def about
